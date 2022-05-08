220cdc82cbd478b2ea544af44c8486d25d9d6138
ranglanimanish90 <ranglani.manish90@gmail.com>
2022-05-08 11:50:29 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot12!";
+        return "Hello from Spring Boot!";
     }   
 }
